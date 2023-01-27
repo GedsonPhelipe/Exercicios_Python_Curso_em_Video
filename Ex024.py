640a2ee5d7142213db9c3adb66226dc06c44800a
Gedson Phelipe <phelipe.santos@protonmail.com>
2023-01-27 14:03:10 -0300

    adição ultima linha.

--- Ex024.py
+++ Ex024.py
@@ -1,5 +1,7 @@
 #Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
 
 cidade = str(input("Digite o nome da cidade em que nasceu: ")).strip().upper()
-print(cidade[0:5] == "SANTO")
-print(cidade)
+if cidade[0:5] == "SANTO":
+    print('Sua cidade começa com a palavra "Santo"')
+else:
+    print('Sua cidade não começa com a palavra "Santo"')
