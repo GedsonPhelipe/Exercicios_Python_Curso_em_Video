8e772755a9d6480c381f397c8a57f5afb85510a7
Gedson Phelipe <88845082+GedsonPhelipe@users.noreply.github.com>
2023-02-13 13:52:35 -0300

    Create Ex051.py

---
+++ Ex051.py
@@ -0,0 +1,12 @@
+# Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
+# No final, mostre os 10 primeiros termos dessa progressão.
+
+t1 = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA em questão: '))
+formula = t1 + (10-1) * razao
+cont = 0
+
+for c in range(t1, formula + razao, razao):
+    cont = cont +1
+    
+    print(f'{cont}° termo é {c}')
