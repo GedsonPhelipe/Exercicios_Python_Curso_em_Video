90fb2baef69973d3710d3a5a536a13d5bca64729
Gedson Phelipe <88845082+GedsonPhelipe@users.noreply.github.com>
2023-02-13 00:06:03 -0300

    Create Ex047.py

---
+++ Ex047.py
@@ -0,0 +1,8 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50. 
+
+from time import sleep
+
+for c in range(2,52, 2):
+    print(c)
+    sleep(0.5)
+print('Acabou!')
