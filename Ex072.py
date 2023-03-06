1c8e1e44a5b9d1f1b0d91fed4d3e4236e373d709
Gedson Phelipe <88845082+GedsonPhelipe@users.noreply.github.com>
2023-03-05 22:09:36 -0300

    Create Ex072.py

---
+++ Ex072.py
@@ -0,0 +1,13 @@
+# Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+        'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+    opcao = int(input('Digite o número desejado de 0 à 20: '))
+    if opcao >= 0 and opcao <= 20:
+        break
+    print('Número inválido,tente novamente!')
+
+print(f'Você digitou o número {cont[opcao]}')
