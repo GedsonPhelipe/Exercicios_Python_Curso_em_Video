3706c6344f87dba75c5d1a0bb57976a9d2a22ecc
Gedson Phelipe <88845082+GedsonPhelipe@users.noreply.github.com>
2024-02-16 18:03:08 -0300

    "Adicionado linha 4"

--- Ex025.py
+++ Ex025.py
@@ -1,6 +1,7 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 
 nome = str(input("Qual seu nome completo? ")).strip().upper()
+
 if 'SILVA' in nome:
     print('Seu nome contém a palavra "Silva"')
 else:
